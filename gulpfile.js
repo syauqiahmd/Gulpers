962e31b14e5641485133f634c034390582f0ec00
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-11-05 16:10:40 +0800

    setting sass + css watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
-var gulp				= require('gulp');
-var browserSync = require('browser-sync').create();
-var sass 				= require('gulp-sass');
+var gulp				  = require('gulp'),
+    sass 				  = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    browserSync   = require('browser-sync');
+
+// sass compiler
+gulp.task('sass', function() {
+  return gulp.src("dist/sass/**/*.scss")
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("app/css"));
+});
 
 // serve task
 gulp.task('serve', ['sass'], function() {
 
   browserSync.init({
     server: {
-      baseDir: "./app"
+      baseDir: "./app/"
     }
   });
 
   gulp.watch("dist/sass/**/*.scss", ['sass']);
+  gulp.watch("app/css/**/*.css").on('change', browserSync.reload);
   gulp.watch("app/*.html").on('change', browserSync.reload);
 
-});
-
-// sass compiler
-gulp.task('sass', function() {
-  return gulp.src("dist/sass/**/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("app/css"))
-    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
